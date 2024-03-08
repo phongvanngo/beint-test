28dd6009217352d179fb11b0c49045d75db875d4
Phong Ngo <phongvanngo.nvp@gmail.com>
2024-03-08 17:12:37 +0700

    init backend

---
+++ backend/beint-test-api/src/test/java/org/example/beinttestapi/BeintTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.beinttestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeintTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
