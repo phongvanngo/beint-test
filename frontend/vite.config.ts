e6c7126b943df2df953315ddcf8e75c7e01382af
Phong Ngo <phongvanngo.nvp@gmail.com>
2024-03-12 14:01:02 +0700

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4500,
+    open: false,
+    host: true,
+    strictPort: true
+  },
 })
